e80324f4fdff64c1eaa62abf92180e32686c3ac8
Pablo Monteiro <paabloms@live.com>
2018-09-27 23:32:39 -0300

    added routes and minor changes

---
+++ backend/src/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function(server){
+
+    //Definir a url base para todas as rotas
+    const router = express.Router()
+    server.use('/api', router)
+
+    //Rotas do ciclo de pagamento
+    const BillingCycle  = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(router,'/billingCycles');
+
+}
