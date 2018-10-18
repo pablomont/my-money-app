34d8e29ee8884b297b00236d7b3cd64bee0f31dd
Pablo Monteiro <paabloms@live.com>
2018-10-18 16:09:03 -0300

    added component footer and changed custom.css

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2
+            <a href="#" target='_blank'> My money</a>.
+        </strong>
+    </footer>
+)
